31eb29126f85fd345e085af4e243acb41120ecdc
loz19001009 <161129298+loz19001009@users.noreply.github.com>
2024-05-31 19:32:44 +0700

    liststickers.js

--- commands/liststickers.js
+++ commands/liststickers.js
@@ -3,13 +3,13 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('liststickers')
-        .setDescription('List all stickers in the server'),
+        .setDescription('Liệt kê tất cả các sticker trong máy chủ'),
     async execute(interaction) {
         const stickers = interaction.guild.stickers.cache.map(s => `${s} - \`${s.name}\``).join('\n');
 
         const embed = new EmbedBuilder()
-            .setTitle('Server Stickers')
-            .setDescription(stickers || 'No stickers found') // Ensure description is not empty
+            .setTitle('Danh Sách Sticker của Máy Chủ')
+            .setDescription(stickers || 'Không tìm thấy sticker nào') // Đảm bảo mô tả không trống
             .setColor('#0099ff');
 
         await interaction.reply({ embeds: [embed] });
